a4ab0ebbfcb69544704600ff81eb50ff3a3887d6
Jakub <jakub@moonbite.space>
2024-02-01 14:31:01 +0100

    Bring back the required import in macro

--- packages/dex/src/pool.rs
+++ packages/dex/src/pool.rs
@@ -277,7 +277,7 @@ pub enum QueryMsg {
     #[returns(CumulativePricesResponse)]
     CumulativePrices {},
     /// Returns a price history of the given duration
-    #[returns(TwapResponse)]
+    #[returns(crate::oracle::TwapResponse)]
     Twap {
         duration: SamplePeriod,
         /// duration: Day and start_age: 3 means to start from first checkpoint 3 days ago
