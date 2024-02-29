ba3a55215e6654c025de9858ac9092564cad6e55
Kaloyan Gangov <gangov1@gmail.com>
2024-02-29 13:12:13 +0200

    stake: unbond_all - next test done

--- contracts/stake/src/multitest/unbond_all.rs
+++ contracts/stake/src/multitest/unbond_all.rs
@@ -68,62 +68,60 @@ fn single_delegate_unbond_and_claim() {
     assert_eq!(suite.query_total_staked().unwrap(), 25_000u128);
 }
 
-// #[test]
-// fn multiple_delegate_unbond_and_claim_with_unbond_all() {
-//     let user = "user";
-//     let mut suite = SuiteBuilder::new()
-//         .with_unbonding_periods(vec![SEVEN_DAYS, SEVEN_DAYS * 3])
-//         .with_initial_balances(vec![(user, 100_000)])
-//         .with_unbonder(UNBONDER)
-//         .build();
+#[test]
+fn multiple_delegate_unbond_and_claim_with_unbond_all() {
+    let user = "user";
+    let mut suite = SuiteBuilder::new()
+        .with_unbonding_periods(vec![SEVEN_DAYS, SEVEN_DAYS * 3])
+        .with_lp_share_denom("tia".to_string())
+        .with_native_balances("tia", vec![(user, 100_000)])
+        .with_unbonder(UNBONDER)
+        .build();
 
-//     // Delegate half of the tokens for 7 days (default with None).
-//     suite.delegate(user, 50_000u128, SEVEN_DAYS).unwrap();
+    // Delegate half of the tokens for 7 days (default with None).
+    suite.delegate(user, 50_000u128, SEVEN_DAYS).unwrap();
 
-//     // Delegate half of the tokens for 21 days.
-//     suite.delegate(user, 50_000u128, SEVEN_DAYS * 3).unwrap();
+    // Delegate half of the tokens for 21 days.
+    suite.delegate(user, 50_000u128, SEVEN_DAYS * 3).unwrap();
 
-//     // Unbond.
-//     suite.unbond(user, 25_000u128, None).unwrap();
+    // Unbond.
+    suite.unbond(user, 25_000u128, None).unwrap();
 
-//     // Set unbond all flag to true.
-//     suite.execute_unbond_all(UNBONDER).unwrap();
+    // Staking contract has all initial tokens.
+    assert_eq!(suite.query_balance_staking_contract().unwrap(), 100_000u128);
 
-//     // Staking contract has all initial tokens.
-//     assert_eq!(suite.query_balance_staking_contract().unwrap(), 100_000u128);
+    // Tokens in stake are 100_000 minus unbonded.
+    assert_eq!(suite.query_total_staked().unwrap(), 75_000u128);
 
-//     // Tokens in stake are 100_000 minus unbonded.
-//     assert_eq!(suite.query_total_staked().unwrap(), 75_000u128);
+    // Claim is there since made before unbond all.
+    let claims = suite.query_claims(user).unwrap();
+    assert_eq!(claims.len(), 1);
 
-//     // Claim is there since made before unbond all.
-//     let claims = suite.query_claims(user).unwrap();
-//     assert_eq!(claims.len(), 1);
+    suite.update_time(SEVEN_DAYS * 2);
+    suite.claim(user).unwrap();
 
-//     suite.update_time(SEVEN_DAYS * 2);
-//     suite.claim(user).unwrap();
+    assert_eq!(suite.query_staked(user, SEVEN_DAYS).unwrap(), 25_000u128);
 
-//     // User claims only tokens unbonded before unbond all.
-//     assert_eq!(
-//         suite.query_balance_vesting_contract(user).unwrap(),
-//         25_000u128
-//     );
+    // Unbond tokens delegated for 21 days.
+    suite.unbond(user, 25_000u128, SEVEN_DAYS * 3).unwrap();
 
-//     // Unbond tokens delegated for 21 days.
-//     suite.unbond(user, 25_000u128, SEVEN_DAYS * 3).unwrap();
+    let claims = suite.query_claims(user).unwrap();
+    assert_eq!(claims.len(), 1);
 
-//     // No claims
-//     let claims = suite.query_claims(user).unwrap();
-//     assert_eq!(claims.len(), 0);
-
-//     // User has previously claimed tokens + unbonded tokens from 21 days.
-//     assert_eq!(
-//         suite.query_balance_vesting_contract(user).unwrap(),
-//         50_000u128
-//     );
-
-//     // Staking contract has half available tokens.
-//     assert_eq!(suite.query_balance_staking_contract().unwrap(), 50_000u128);
-// }
+    // User has previously claimed tokens + unbonded tokens from 21 days.
+    assert_eq!(
+        suite
+            .query_all_staked(user)
+            .unwrap()
+            .stakes
+            .into_iter()
+            .map(|stake_el| u128::from(stake_el.stake))
+            .sum::<u128>(),
+        50_000u128
+    );
+    // last unbonded by user is 100k - 25k
+    assert_eq!(suite.query_balance_staking_contract().unwrap(), 75_000u128);
+}
 
 // #[test]
 // fn delegate_with_unbond_all_flag() {
